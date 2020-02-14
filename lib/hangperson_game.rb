65ddf358874d90697be4a2a8a803b0b5ac15ce4d
Ubuntu <ubuntu@ip-172-31-80-214.ec2.internal>
2020-02-14 16:14:32 +0000

    Implemented Hangman methods

--- lib/hangperson_game.rb
+++ lib/hangperson_game.rb
@@ -8,10 +8,57 @@ class HangpersonGame
   # def initialize()
   # end
   
+  
+  
   def initialize(word)
     @word = word
+    @guesses = ''
+    @wrong_guesses = ''
+    @inputLetter = ''
   end
-
+  
+  attr_accessor :word
+  attr_accessor :guesses
+  attr_accessor :wrong_guesses
+  
+  def guess(letter)
+    raise ArgumentError if  letter == nil || letter == '' || letter.match(/[^a-zA-Z]/)
+    inputLetter = letter.downcase
+    if not (guesses.include? inputLetter) || (wrong_guesses.include? inputLetter)
+      if word.include? inputLetter
+        guesses << inputLetter
+      else
+        wrong_guesses << inputLetter
+      end
+      #@guessCount = @guessCount - 1
+    else
+      return false
+    end
+  end
+  
+  def check_win_or_lose
+    if guesses.chars.sort.join == word.chars.sort.join
+      return :win
+    elsif wrong_guesses.length == 7
+      return :lose
+    else
+      return :play
+    end
+  end
+  
+  def word_with_guesses
+    displayed = ""
+    word.split("").map do |char|
+      if guesses.include? char
+        #puts "True"
+        displayed << char
+      else 
+        displayed << "-"
+      end
+    end
+    return displayed
+  end
+    
   # You can test it by running $ bundle exec irb -I. -r app.rb
   # And then in the irb: irb(main):001:0> HangpersonGame.get_random_word
   #  => "cooking"   <-- some random word
